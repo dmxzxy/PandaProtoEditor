0c023c5e1966cc58a999bfff16a9c5e18c3f7698
dmxzxy <dmxzxy@126.com>
2018-09-30 09:09:05 +0800

    update

--- protocal/models.py
+++ protocal/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Lock(models.Model):
+	lock_owner = models.CharField(max_length = 150, black = False)
+	lock_timestamp = models.DateTimeField(auto_now_add = True)
+
+class Project(models.Model):
+	title = models.CharField(max_length = 150, black = False)
+	namespace = models.CharField(max_length = 150, blank = False, unique = True)
+	timestamp = models.DateTimeField(auto_now_add = True)
+	def __unicode__(self):
+		return self.title
+
+class ProjectBranche(models.Model):
+    project = models.ForeignKey(Project, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 150, black = False)
+	proto_url = models.CharField(max_length = 150, black = False)
+	timestamp = models.DateTimeField(auto_now_add = True)
+	def __unicode__(self):
+		return self.title
--- protocal/views.py
+++ protocal/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	projects = Project.objects.all().order_by('-timestamp')
+	return render(request, 'index.html',{'projects' : projects})
