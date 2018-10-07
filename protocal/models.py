0e42585dd12a71585d6c5c1057cfa194ed53dad5
sycmore <dmxzxy@126.com>
2018-10-07 23:35:17 +0800

    定义完整models

--- protocal/models.py
+++ protocal/models.py
@@ -9,6 +9,15 @@ class Lock(models.Model):
 	lock_owner = models.CharField(max_length = 150, black = False)
 	lock_timestamp = models.DateTimeField(auto_now_add = True)
 
+# optional required repeated
+class FieldLabel(models.Model):
+    name = models.CharField(max_length = 100, blank = False)
+    desc = models.CharField(max_length = 150, blank = False)
+    def __unicode__(self):
+        return self.name
+
+#---------------------------------------------------------------------------------
+#         
 class Project(models.Model):
 	title = models.CharField(max_length = 150, black = False)
 	namespace = models.CharField(max_length = 150, blank = False, unique = True)
@@ -19,10 +28,10 @@ class Project(models.Model):
 class ProjectBranche(models.Model):
     project = models.ForeignKey(Project, on_delete = models.CASCADE)
     title = models.CharField(max_length = 150, black = False)
-	proto_url = models.CharField(max_length = 150, black = False)
-	timestamp = models.DateTimeField(auto_now_add = True)
-	def __unicode__(self):
-		return self.title
+    proto_url = models.CharField(max_length = 150, black = False)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    def __unicode__(self):
+        return self.title
 
 class Module(models.Model):
     project = models.ForeignKey(ProjectBranche, on_delete = models.CASCADE)
@@ -32,33 +41,77 @@ class Module(models.Model):
     def __unicode__(self):
         return self.name
 
-class Protocal(models.Model):
+
+#// 0 is reserved for errors.
+#// Order is weird for historical reasons.
+#TYPE_DOUBLE         = 1;
+#TYPE_FLOAT          = 2;
+#// Not ZigZag encoded.  Negative numbers take 10 bytes.  Use TYPE_SINT64 if
+#// negative values are likely.
+#TYPE_INT64          = 3;
+#TYPE_UINT64         = 4;
+#// Not ZigZag encoded.  Negative numbers take 10 bytes.  Use TYPE_SINT32 if
+#// negative values are likely.
+#TYPE_INT32          = 5;
+#TYPE_FIXED64        = 6;
+#TYPE_FIXED32        = 7;
+#TYPE_BOOL           = 8;
+#TYPE_STRING         = 9;
+#TYPE_GROUP          = 10;  // Tag-delimited aggregate.
+#TYPE_MESSAGE        = 11;  // Length-delimited aggregate.
+
+# New in version 2.
+#TYPE_BYTES          = 12;
+#TYPE_UINT32         = 13;
+#TYPE_ENUM           = 14;
+#TYPE_SFIXED32       = 15;
+#TYPE_SFIXED64       = 16;
+#TYPE_SINT32         = 17;  // Uses ZigZag encoding.
+#TYPE_SINT64         = 18;  // Uses ZigZag encoding
+
+class FieldType(models.Model):
+    project = models.ForeignKey(ProjectBranche, on_delete = models.CASCADE)
+    type = models.IntegerField()
+    typename = models.CharField(max_length = 150)
+    def __unicode__(self):
+        return self.typename
+
+class Message(models.Model):
     module = models.ForeignKey(Module, on_delete = models.CASCADE, null = True, default = None)
     name = models.CharField(max_length = 100, blank = False)
     fullname = models.CharField(max_length = 100, blank = False)
     desc = models.CharField(max_length = 150, blank = False)
-    nested = models.ForeignKey(Protocal, null = True, default = None)
+    nested = models.ForeignKey("Message", null = True, default = None)
     timestamp = models.DateTimeField(auto_now_add = True)
     def __unicode__(self):
         return self.name
 
-class SegmentLabel(models.Model):
+class Field(models.Model):
+    message = models.ForeignKey(Message, on_delete = models.CASCADE)
     name = models.CharField(max_length = 100, blank = False)
+    fullname = models.CharField(max_length = 150, blank = False)
     desc = models.CharField(max_length = 150, blank = False)
+    label = models.ForeignKey(FieldLabel)
+    type = models.ForeignKey(FieldType)
+    number = models.IntegerField()
     def __unicode__(self):
         return self.name
 
-class SegmentType(models.Model):
-	type = models.IntegerField()
-	
-
-class Segment(models.Model):
-    protocal = models.ForeignKey(Protocal, on_delete = models.CASCADE)
+class Enum(models.Model):
+    module = models.ForeignKey(Module, on_delete = models.CASCADE, null = True, default = None)
     name = models.CharField(max_length = 100, blank = False)
-    fullname = models.CharField(max_length = 100, blank = False)
+    fullname = models.CharField(max_length = 150, blank = False)
     desc = models.CharField(max_length = 150, blank = False)
-    label = models.ForeignKey(SegmentLabel)
+    nested = models.ForeignKey("Message", null = True, default = None)
+    timestamp = models.DateTimeField(auto_now_add = True)
+    def __unicode__(self):
+        return self.name
+
+class EnumValue(models.Model):
+    enum = models.ForeignKey(Enum, on_delete = models.CASCADE, null = True, default = None)
+    name = models.CharField(max_length = 100, blank = False)
+    fullname = models.CharField(max_length = 150, blank = False)
+    desc = models.CharField(max_length = 150, blank = False)
     number = models.IntegerField()
-    type = 
     def __unicode__(self):
         return self.name
