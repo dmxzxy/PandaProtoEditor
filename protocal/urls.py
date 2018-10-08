ddabcb0e62534e2fdd70a3a4000bdc0d5fdbc092
dmxzxy <dmxzxy@126.com>
2018-10-08 20:20:20 +0800

    日常提交

--- protocal/urls.py
+++ protocal/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^project/create/$', views.project_create, name='project_create'),
+    url(r'^project/create/branche/$', views.project_create_branche, name='project_create_branche'),
+    url(r'^project/(?P<project_id>[0-9]+)/$', views.project_detail, name='project_detail'),
 ]
--- protocal/views.py
+++ protocal/views.py
@@ -3,8 +3,54 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+import os
+from django.core.exceptions import ValidationError
+from django.db.models.query_utils import Q
+from django.forms.models import ModelForm
+from django.http.response import HttpResponse, StreamingHttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template import RequestContext  
+
+from datetime import datetime
+from string import strip
+
+from protocal.models import *
+
 # Create your views here.
 
 def index(request):
-	projects = Project.objects.all().order_by('-timestamp')
-	return render(request, 'index.html',{'projects' : projects})
+	raw_projects = Project.objects.all().order_by('-timestamp')
+	projects = []
+	for project in raw_projects:
+		projects.append({
+			'project' : project,
+			'branches' : ProjectBranche.objects.all().filter(project = project)
+		})		
+	return render(request, 'index.html', {'projects' : projects})
+
+
+def project_create(request):
+	if request.method == 'POST': 
+		project_name = request.POST['project_name'] 
+		project_namespace = request.POST['project_namespace'] 
+		
+		if project_name and project_namespace:
+			project = Project(title = strip(project_name), namespace = strip(project_namespace))
+			project.save()
+		else:
+			raise ValidationError("name or namespace should not be null.")
+
+		return HttpResponse("<h1>Created successfully .</h1>")  
+	else:
+		return HttpResponse("<h1>Invalid request.</h1>")  
+
+def project_create_branche(request):
+	if request.method == 'POST': 
+
+		return HttpResponse("<h1>Created successfully .</h1>")  
+	else:
+		return HttpResponse("<h1>Invalid request.</h1>")  
+
+
+def project_detail(request, project_id):
+	return HttpResponse("Hello， World!")
